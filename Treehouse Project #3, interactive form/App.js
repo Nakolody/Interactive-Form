f060e794e6895ffb9270808c56180f3b700e803c
Nakolody <48574153+Nakolody@users.noreply.github.com>
2019-07-06 23:01:46 -0400

    Add files via upload
    Updated drop down lists and index.html

--- Treehouse Project #3, interactive form/App.js
+++ Treehouse Project #3, interactive form/App.js
@@ -1,1 +1,31 @@
+const otherTitle = document.querySelector('#other-title');
+//Sets initial focus on name field for filling out form
 $('#name').focus();
+//Hides the job role title initially
+$('#other-title').hide();
+//Function to show text field when other Title is selected
+$('#title').on('change', function(e){
+    if($('#title option:selected').val() === 'other'){
+        $('#other-title').show();
+    }else {
+        $('#other-title').hide();
+    }
+});
+//Displays the appropriate T-shirt colors via drop box
+$('#design').on('change', function(e){
+    for (let i = 0; i <= $('#color option').length; i +=1){
+        $('#color option').eq(i).prop('disabled',false);
+    }
+    if($('#design option:selected').val() === 'js puns'){
+        for (let js = 0; js <=2; js +=1){
+            $('#color option').eq(js).prop('disabled',true);
+            $('#color option').eq(3).attr('selected',true);
+        }
+    }
+    if($('#design option:selected').val() === 'heart js'){
+        for (let hjs =3; hjs <=6; hjs +=1){
+            $('#color option').eq(hjs).prop('disabled',true);
+            $('#color option').eq(0).attr('selected',true);
+        }
+    }
+});
